a0d74833e6e7221b26f8b0d1b6940451b0dc78c1
Cocola6s6 <799497008@qq.com>
2023-05-24 10:07:51 +0800

    update: 修改项目结构

--- gpt-java/src/main/java/com/cocola/gpt/GptJavaApplication.java
+++ gpt-java/src/main/java/com/cocola/gpt/GptJavaApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-public class GptFirstApplication {
+public class GptJavaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GptFirstApplication.class, args);
+        SpringApplication.run(GptJavaApplication.class, args);
     }
 
 }
